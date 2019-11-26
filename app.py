8fb563731097fc3dd6154a0b224af23d6cf46efb
Kevin Sattakun <42414822+kevsatt@users.noreply.github.com>
2019-11-26 13:53:21 -0800

    Add files via upload
    test flask deployment

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, abort, render_template, send_file
+from fastai.vision import *
+from fastai.utils.mem import *
+from fastai.callbacks.hooks import *
+import cv2
+import traceback
+
+class SegLabelListCustom(SegmentationLabelList):
+    def open(self, fn): return open_mask(fn, convert_mode='L')
+
+
+class SegItemListCustom(SegmentationItemList):
+    _label_cls = SegLabelListCustom
+
+def acc_image_seg(input, target):
+    target = target.squeeze(1)
+    mask = target != void_code
+    return (input.argmax(dim=1)[mask] == target[mask]).float().mean()
+
+learn = load_learner(Path(), 'unet_eye_seg_model.pkl')
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """REST endpoint for serving upload form then calling Justin's code"""
+    if request.method == 'GET':
+        """First, serving input form as a webpage"""
+        return render_template('index.html')
+    else:
+        """Otherwise this is a POST to call the imported function"""
+        print(request.files['file'])
+        data = request.files['file']
+        filename = data.filename
+        data.save(filename)
+        try:
+            # test = Image(data)
+            img = open_image(filename, convert_mode='L')
+            prediction = learn.predict(img)
+            # prediction[0].show(figsize=(5, 5))
+            mask = prediction[0]
+            mask.save('temp.png')
+            mask_output = cv2.imread('temp.png')
+            test = cv2.cvtColor(mask_output, cv2.COLOR_BGR2RGB)
+            test2.save('test.png')
+            return send_file('test.png', mimetype='image/gif')
+        except Exception as e:
+            traceback.print_exc()
+            abort(500, str(e))
+
+if __name__ == '__main__':
+    app.run()
